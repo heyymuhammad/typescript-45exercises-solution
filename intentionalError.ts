972bdc991349c727650fcbc991ee9c4a874eb8e9
heyysultan <sultan@linkedunion.com>
2024-02-18 15:27:17 +0500

    added missing exercise

---
+++ intentionalError.ts
@@ -0,0 +1,5 @@
+/* Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. 
+Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program. */
+
+let index_error_array: string[] = ["Boston", "Harvard", "MIT"]
+console.log(index_error_array[3])
